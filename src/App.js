fde373afcb579fab7b92b7433406c03024b666e5
Phumtham <98636379+Oatttttttt@users.noreply.github.com>
2024-01-28 13:32:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import DataTable from "react-data-table-component";
+import axios from "axios";
+
+const columns = [
+  {
+    name: "Name",
+    selector: (row) => row.name,
+  },
+  {
+    name: "Nasa-jpl-url",
+    selector: (row) => row.nasa_jpl_url,
+  },
+  {
+    name: "Absolute magnitude h",
+    selector: (row) => row.absolute_magnitude_h,
+  },
+];
 
 function App() {
+  const [NeoWs, setNeoWs] = useState([]);
+
+  const url_api = process.env.REACT_APP_API_KEY;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url_api);
+        if (response.status === 200) {
+          const data = response.data;
+          const neoData = Object.values(data.near_earth_objects).flat();
+          setNeoWs(neoData);
+        } else {
+          console.error("Error fetching NEO data");
+        }
+      } catch (error) {
+        console.error("Error fetching NEO data:", error);
+      }
+    };
+
+    fetchData();
+  }, [url_api]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h3 style={{ textAlign: "center" }}>
+        APIs from :{" "}
+        <a href="https://api.nasa.gov/" target="_blank" rel="noreferrer">
+          https://api.nasa.gov/
         </a>
-      </header>
+      </h3>
+      <DataTable columns={columns} data={NeoWs} pagination />
     </div>
   );
 }
